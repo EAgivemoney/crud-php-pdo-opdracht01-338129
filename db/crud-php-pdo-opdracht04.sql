cad9b346519d323c29062a9aad887d882160a0b4
EAgivemoney <thomasmeijer07@t-mobilethuis.nl>
2024-04-22 15:32:34 +0200

    SQL toegevoegt

---
+++ db/crud-php-pdo-opdracht04.sql
@@ -0,0 +1,49 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Gegenereerd op: 22 apr 2024 om 13:31
+-- Serverversie: 8.0.31
+-- PHP-versie: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `nailstudio`
+--
+CREATE DATABASE IF NOT EXISTS `nailstudio` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE `nailstudio`;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `afspraak`
+--
+
+DROP TABLE IF EXISTS `afspraak`;
+CREATE TABLE IF NOT EXISTS `afspraak` (
+  `Id` int NOT NULL AUTO_INCREMENT,
+  `Kleur1` varchar(10) NOT NULL,
+  `Kleur2` varchar(10) NOT NULL,
+  `Kleur3` varchar(10) NOT NULL,
+  `Kleur4` varchar(10) NOT NULL,
+  `Telefoon_nummer` varchar(20) NOT NULL,
+  `Email_adres` varchar(50) NOT NULL,
+  `Datum_afspraak` datetime NOT NULL,
+  `Behandeling` varchar(100) NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- index.php
+++ index.php
@@ -6,20 +6,22 @@
     <title>Document</title>
 </head>
 <body>
-    <h2>Bling Bling Nail Studio Chantal</h2>
-    <p>Kies 4 basiskleuren voor uw nagels:</p>
-    <input type="color" name="" id=""> <input type="color" name="" id=""> <input type="color" name="" id=""> <input type="color" name="" id="">
-    <p>Uw telefoonnummer:</p>
-    <input type="tel" name="" id="">
-    <p>Uw e-mailadres:</p>
-    <input type="email" name="randomguy@nowhere.com" id="">
-    <p>Afspraak datum:</p>
-    <input type="datetime-local" name="" id="">
-    <p>Soort behandeling:</p>
-    <input type="checkbox" name="" id=""> Nagelbijt arrangement (termijnbetaling mogelijk) €180<br>
-    <input type="checkbox" name="" id=""> Luxe manicure (massage en handpakking) €30,00<br>
-    <input type="checkbox" name="" id=""> Nagelreparatie per nagel (in eerste week gratis) €5,00<br>
-    <button type="submit">Sla op</button>
-    <button type="reset">Reset</button>
+    <form action="" method="post">
+        <h2>Bling Bling Nail Studio Chantal</h2>
+        <p>Kies 4 basiskleuren voor uw nagels:</p>
+        <input type="color" name="" id=""> <input type="color" name="" id=""> <input type="color" name="" id=""> <input type="color" name="" id="">
+        <p>Uw telefoonnummer:</p>
+        <input type="tel" name="" id="">
+        <p>Uw e-mailadres:</p>
+        <input type="email" name="" id="">
+        <p>Afspraak datum:</p>
+        <input type="datetime-local" name="" id="">
+        <p>Soort behandeling:</p>
+        <input type="checkbox" name="" id=""> Nagelbijt arrangement (termijnbetaling mogelijk) €180<br>
+        <input type="checkbox" name="" id=""> Luxe manicure (massage en handpakking) €30,00<br>
+        <input type="checkbox" name="" id=""> Nagelreparatie per nagel (in eerste week gratis) €5,00<br>
+        <button type="submit">Sla op</button>
+        <button type="reset">Reset</button>
+    </form>
 </body>
 </html>
