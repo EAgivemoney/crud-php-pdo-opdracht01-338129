60f515c15053a3e6bfe4f6a9cc7b6aa17e07dba1
EAgivemoney <thomasmeijer07@t-mobilethuis.nl>
2024-04-22 15:07:24 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Bling Bling Nail Studio Chantal</h2>
+    <p>Kies 4 basiskleuren voor uw nagels:</p>
+    <input type="color" name="" id=""> <input type="color" name="" id=""> <input type="color" name="" id=""> <input type="color" name="" id="">
+    <p>Uw telefoonnummer:</p>
+    <input type="tel" name="" id="">
+    <p>Uw e-mailadres:</p>
+    <input type="email" name="" id="">
+    <p>Afspraak datum:</p>
+    <input type="datetime-local" name="" id="">
+    <p>Soort behandeling:</p>
+</body>
+</html>
